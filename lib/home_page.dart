b50bf06a136954aa639f94714c136e68f407b2c2
Aman_Sensei <anautiyal9876@gmail.com>
2023-01-27 15:49:41 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40.0,
+          ),
+          Image.asset(
+            'assets/images/homepage_image.png',
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Let's make you see your imaginations!",
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 24,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    iconColor: Colors.black,
+                    hintText: 'Enter text prompt to generate image',
+                    labelText: 'Type text here',
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  child: Text('Generate'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:fresco/home_page.dart';
 
 void main() {
   runApp(AppWidget());
 }
 
 class AppWidget extends StatelessWidget {
+  bool login = false;
+  bool signup = false;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('AppBar_Text'),
-        ),
-        body: Text('Body_Text'),
+    return MaterialApp(  
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      routes: {
+        "/": (context) => HomePage(), // Home Page
+        "/": (context) => HomePage(), // Login Page
+        "/": (context) => HomePage(), // Sign Up Pagefile
+      },
     );
   }
 }
